a116fa83595049db2ed864646256ff4dc238adc6
Stephen <stephen.lu20@gmail.com>
2026-01-05 17:17:23 -0500

    Added error detail dto

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/dtos/ErrorDetail.java
@@ -0,0 +1,28 @@
+package io.zipcoder.tc_spring_poll_application.dtos.error;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class ErrorDetail {
+    private String title;
+    private int status;
+    private String detail;
+    private long timeStamp;
+    private String developerMessage;
+    private Map<String, List<ValidationError>> errors;
+
+    // getters and setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
+    public String getDetail() { return detail; }
+    public void setDetail(String detail) { this.detail = detail; }
+    public long getTimeStamp() { return timeStamp; }
+    public void setTimeStamp(long timeStamp) { this.timeStamp = timeStamp; }
+    public String getDeveloperMessage() { return developerMessage; }
+    public void setDeveloperMessage(String developerMessage) { this.developerMessage = developerMessage; }
+    public Map<String, List<ValidationError>> getErrors() { return errors; }
+    public void setErrors(Map<String, List<ValidationError>> errors) { this.errors = errors; }
+}
