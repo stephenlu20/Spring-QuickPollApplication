d18771dda9d0cb3a35b57063dad71c4249e5e6ca
Stephen <stephen.lu20@gmail.com>
2026-01-05 16:24:42 -0500

    Added entities and repositories

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -0,0 +1,31 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "OPTION_ID")
+    private Long id;
+
+    @Column(name = "OPTION_VALUE")
+    private String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -0,0 +1,50 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+@Entity
+public class Poll {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "POLL_ID")
+    private Long id;
+
+    @Column(name = "QUESTION")
+    @NotEmpty
+    private String question;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "POLL_ID")
+    @OrderBy
+    @Size(min = 2, max = 6)
+    private Set<Option> options;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Set<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Set<Option> options) {
+        this.options = options;
+    }
+}
